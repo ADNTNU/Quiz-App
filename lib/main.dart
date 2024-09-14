a012ec99d103abd50373a7c704d2a2d0b921b774
Daniel Neset <danielneset@gmail.com>
2024-09-14 03:05:39 +0200

    Added an image, text and button to start page

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,18 @@ import 'package:quiz_app/start_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.deepPurple, Colors.deepOrange],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
       ),
     ),
   );
